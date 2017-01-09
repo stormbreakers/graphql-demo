49ef84145e8b9a5bef3e4dcfa5ee1e4b5222a606
Joy Koli <joykoli5@gmail.com>
2017-01-09 12:49:16 +0530

    20170109

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
+import schema from './schema.js';
 
 const app = express();
 
 app.use(graphQLHTTP({
+    schema,
     graphiql: true,
 }));
 
